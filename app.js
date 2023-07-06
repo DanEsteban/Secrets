e36b3e3473ad912dc1d61ec82da0638d6bc797f2
Daniel Velasco <desteban.velpi@gmail.com>
2023-07-06 12:41:56 -0500

    Aqui use como hash el md5

--- app.js
+++ app.js
@@ -4,8 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
-const saltRounds=10;
+const md5 = require("md5");
 
 const app = express();
 
@@ -41,39 +40,31 @@ app.get("/register", function(req, res){
 });
 
 app.post("/register", function(req, res){
-    
-    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
-        const newUser = new User({
-            email: req.body.username,
-            password: hash
-        });
-    
-        newUser.save()
-        .then(() => {
-            res.render("secrets");
-        })
-        .catch((err) => {
-            console.log(err);
-        });
+    const newUser = new User({
+        email: req.body.username,
+        password: md5(req.body.password)
     });
 
-    
+    newUser.save()
+    .then(() => {
+        res.render("secrets");
+    })
+    .catch((err) => {
+        console.log(err);
+    });
 });
 
 app.post("/login", function(req, res){
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
 
     User.findOne({email: username})
     .then((foundUser) => {
-        bcrypt.compare(password, foundUser.password, function(err, result) {
-            if (result === true) {
-                res.render("secrets");
-            }else {
-                res.redirect("/login");
-            }
-            
-        });
+        if (foundUser.password === password) {
+            res.render("secrets");
+        } else {
+            res.redirect("/login");
+        }
     })
     .catch((err) => {
         console.log(err);
